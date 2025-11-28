f1822604bc3bb7c594341eb3f1068eec734fa760
anoudabdusalam <anoud.salah2003@gmail.com>
2025-11-28 17:05:27 +0200

    Add files via upload

---
+++ BorrowRecord.java
@@ -0,0 +1,43 @@
+package com.library.domain;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+public class BorrowRecord implements Serializable {
+
+    private String bookTitle;
+    private String isbn;
+    private String type;     // BOOK or CD
+    private LocalDate borrowDate;
+    private LocalDate deadline;
+
+    public BorrowRecord(String bookTitle, String isbn, String type, LocalDate borrowDate, LocalDate deadline) {
+        this.bookTitle = bookTitle;
+        this.isbn = isbn;
+        this.type = type;
+        this.borrowDate = borrowDate;
+        this.deadline = deadline;
+    }
+
+    // -------- getters --------
+
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public LocalDate getBorrowDate() {
+        return borrowDate;
+    }
+
+    public LocalDate getDeadline() {
+        return deadline;
+    }
+}
